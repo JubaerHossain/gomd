cc71aee69f623a22e8624814591bac2ed93ba2cb
JubaerHossain <jubaer01.cse@gmail.com>
2023-08-20 23:08:13 +0600

    file creation

---
+++ command/create.go
@@ -0,0 +1,132 @@
+package command
+
+import (
+	"errors"
+	"log"
+	"path"
+	"runtime"
+	"strings"
+
+	"github.com/gertd/go-pluralize"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+const (
+	AppRoot        = "app"
+	ControllersDir = "controllers"
+	ModelsDir      = "models"
+	RoutesDir      = "routes"
+	ServicesDir    = "services"
+	RepositoryDir  = "repository"
+	TemplateDir    = "template"
+)
+
+var AppName string
+
+var CLI = &cobra.Command{
+	Use:  "gomd",
+	Args: cobra.MinimumNArgs(2),
+	RunE: Run,
+}
+
+func Run(cmd *cobra.Command, args []string) error {
+	if len(args) < 2 {
+		return errors.New("not enough arguments")
+	}
+
+	AppName = args[0]
+	name := args[1]
+	fs := afero.NewBasePathFs(afero.NewOsFs(), AppRoot+"/")
+	if err := createFolders(fs, name); err != nil {
+		return err
+	}
+	if err := createFiles(fs, name); err != nil {
+		return err
+	}
+	return nil
+}
+
+func createFolders(fs afero.Fs, name string) error {
+	dirs := []string{ControllersDir, ModelsDir, RoutesDir, ServicesDir, RepositoryDir}
+	for _, dir := range dirs {
+		if err := fs.Mkdir(path.Join(name, dir), 0755); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func createFiles(fs afero.Fs, name string) error {
+	createFile(fs, name, path.Join(TemplateDir, "controller.stub"), path.Join(name, ControllersDir, name+"_controller.go"))
+	createFile(fs, name, path.Join(TemplateDir, "model.stub"), path.Join(name, ModelsDir, name+".go"))
+	createFile(fs, name, path.Join(TemplateDir, "route.stub"), path.Join(name, RoutesDir, "api.go"))
+	createFile(fs, name, path.Join(TemplateDir, "repository.stub"), path.Join(name, "repository", name+"_repository.go"))
+	createFile(fs, name, path.Join(TemplateDir, "service.stub"), path.Join(name, ServicesDir, name+"_service.go"))
+	return nil
+}
+
+func createFile(fs afero.Fs, name, stubPath, filePath string) error {
+	fs.Create(filePath)
+
+	_, filename, _, _ := runtime.Caller(1)
+	stubPath = path.Join(path.Dir(filename), stubPath)
+
+	contents, err := fileContents(stubPath)
+	if err != nil {
+		return err
+	}
+	contents = replaceStub(contents, name)
+
+	if err := overwrite(AppRoot+"/"+filePath, contents); err != nil {
+		return err
+	}
+	return nil
+}
+
+func fileContents(file string) (string, error) {
+	a := afero.NewOsFs()
+	contents, err := afero.ReadFile(a, file)
+	if err != nil {
+		return "", err
+	}
+	return string(contents), nil
+}
+
+func overwrite(file string, message string) error {
+	a := afero.NewOsFs()
+	return afero.WriteFile(a, file, []byte(message), 0666)
+}
+
+func replaceStub(content string, name string) string {
+	content = strings.Replace(content, "{{TitleName}}", Title(name), -1)
+	content = strings.Replace(content, "{{PluralLowerName}}", Lower(Plural(name)), -1)
+	content = strings.Replace(content, "{{SingularLowerName}}", Lower(Singular(name)), -1)
+	content = strings.Replace(content, "{{AppName}}", AppName, -1)
+	content = strings.Replace(content, "{{AppRoot}}", AppRoot, -1)
+	return content
+}
+
+func Plural(name string) string {
+	pluralize := pluralize.NewClient()
+	return pluralize.Plural(name)
+}
+
+func Singular(name string) string {
+	pluralize := pluralize.NewClient()
+	return pluralize.Singular(name)
+}
+
+func Lower(name string) string {
+	return strings.ToLower(name)
+}
+
+func Title(name string) string {
+	return strings.Title(Lower(name))
+}
+
+func main() {
+	if err := CLI.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -2,38 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/JubaerHossain/gomd/config"
-	"github.com/spf13/cobra"
+	"github.com/JubaerHossain/gomd/gomd"
 )
 
-var asciiArt = `
-     ____ _____  ____ ___  ____/ /
-    / __ / __ \/ __  __ \/ __  / 
-   / /_/ / /_/ / / / / / / /_/ /  
-   \__, /\____/_/ /_/ /_/\__,_/   
-  /____/                          
-`
 
-
-// Create a Cobra command for gomd
-var cmd = &cobra.Command{
-	Use:   "gomd",
-	Short: asciiArt,
-	Long:  "gomd is a sleek and powerful markdown server designed to elegantly serve markdown files over HTTP.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(asciiArt)
-		fmt.Println("✨ A CLI tool for building golang restful API application. ✨")
-	},
-	Version: config.Version,
-
-}
 
 // run is the main function to execute the gomd command
 func main() {
-	// Execute the Cobra command
-	if err := cmd.Execute(); err != nil {
-		log.Fatal(err)
+	err := gomd.Run()
+	if err != nil {
+		fmt.Println("execute error: ", err.Error())
 	}
 }
